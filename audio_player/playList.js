174b5f1fe8aff98e394c2385c8a398aaa0f156c6
Voitko Viacheslav Ivanovich <voitko-viacheslav-ivanovich1992@ukr.net>
2023-09-24 23:24:56 +0300

    feat: add blur, player, control, time, volume ,title

---
+++ audio_player/playList.js
@@ -0,0 +1,27 @@
+const playList = [
+  {
+    name: 'DJ Goja',
+    title: 'Calm down',
+    img: './assets/img/dj.jpg',
+    src: './assets/sounds/DJ Goja.mp3',
+  },
+  {
+    name: 'Alan Walker',
+    title: 'Force',
+    img: './assets/img/alan.jpg',
+    src: './assets/sounds/Force.mp3',
+  },
+  {
+    name: 'LP',
+    title: 'In the end',
+    img: './assets/img/lp.jpg',
+    src: './assets/sounds/In the end.mp3',
+  },
+  {
+    name: 'Kuzma',
+    title: 'Mom',
+    img: './assets/img/kuzma.jpg',
+    src: './assets/sounds/Mom.mp3',
+  },
+];
+export default playList;
